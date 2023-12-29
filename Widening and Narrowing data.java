1f36b5a629ae95973b9e1bd3d61abde0fa409fa7
Sripathy R S <123567370+sripathy914@users.noreply.github.com>
2023-12-29 09:23:50 +0530

    Create Widening and Narrowing data.java

---
+++ Widening and Narrowing data.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		char c=sc.next().charAt(0);
+		int n=sc.nextInt();
+		
+		int c1=c;
+		char n1=(char)n;
+		System.out.println(c1);
+		System.out.println(n1);
+	}
+}
