44a0af2d91a651748ba1b8dcaeee13c35f56ed31
Sripathy R S <123567370+sripathy914@users.noreply.github.com>
2024-01-02 09:10:58 +0530

    Create String rotation.java

---
+++ String rotation.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    Scanner sc=new Scanner(System.in);
+	    String s1=sc.next(),s2=sc.next();
+	    s1=s1+s2;
+	    System.out.println(s1.contains(s2));
+	}
+}
+
+
+//contains
+//equals
