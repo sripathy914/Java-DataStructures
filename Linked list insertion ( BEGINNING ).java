753bb96ee1e10d995ff9b6e86008ac96995a5d18
Sripathy R S <123567370+sripathy914@users.noreply.github.com>
2024-01-03 08:58:24 +0530

    Create Linked list insertion ( BEGINNING ).java

---
+++ Linked list insertion ( BEGINNING ).java
@@ -0,0 +1,76 @@
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    
+    public Node(int data)
+    {
+        this.data=data;
+        this.next=null;
+    }
+}
+
+class Linkedlist
+{
+    Node head;
+    public Linkedlist()
+    {
+        this.head=null;
+    }
+    
+    public void append(int data)
+    {
+        Node newnode=new Node(data);
+        if(this.head==null)
+        {
+            this.head=newnode;
+            return;
+        }
+        
+        Node lastnode=this.head;
+        while(lastnode.next!=null)
+        {
+            lastnode=lastnode.next;
+        }
+        lastnode.next=newnode;
+    }
+    
+    public void printlist()
+    {
+        Node curr=this.head;
+        while(curr!=null)
+        {
+            System.out.println(curr.data);
+            curr=curr.next;
+        }
+    }
+    
+    public void del_begg()
+    {
+        Node temp=this.head;
+        this.head=this.head.next;
+        temp.next=null;
+    }
+    
+}
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        Linkedlist l=new Linkedlist();
+        for(int i=1;i<6;i++)
+        {
+            l.append(i);
+        }
+        l.printlist();
+        
+        System.out.println("\nAfter deletion of beginning...");
+        l.del_begg();
+        l.printlist();
+    }
+}
+
+
+
