9f07ba048e45dd7f55fe05d6a8615effac3dadb6
Sripathy R S <123567370+sripathy914@users.noreply.github.com>
2024-01-05 09:03:03 +0530

    Create Insertion of linked list- Beginning.java

---
+++ Insertion of linked list- Beginning.java
@@ -0,0 +1,56 @@
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    
+    public Node(int data)
+    {
+        this.data=data;
+        this.next=null;
+    }
+}
+
+class Linkedlist
+{
+    Node head;
+    public Linkedlist()
+    {
+        this.head=null;
+    }
+    
+    public void append(int data)
+    {
+        Node newnode=new Node(data);
+        if(head==null)
+        {
+            this.head=newnode;
+            return;
+        }
+        newnode.next=this.head;
+        this.head=newnode;
+    }
+    
+    public void printlist()
+     {
+        Node curr=this.head;
+        while(curr!=null)
+        {
+            System.out.println(curr.data);
+            curr=curr.next;
+        }
+    }
+}
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        Linkedlist l=new Linkedlist();
+        for(int i=1;i<6;i++)
+        l.append(i);
+        l.printlist();
+    }
+}
+
+
